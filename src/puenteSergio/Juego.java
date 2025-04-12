aadaacce1f3531ce6d4fce3fcbf6618d7bad912a
Sergio <sergio.puente@alumnos.uneatlantico.es>
2025-04-12 16:34:52 +0000

    Corrección

--- src/puenteSergio/Juego.java
+++ src/puenteSergio/Juego.java
@@ -3,6 +3,6 @@ package puenteSergio;
 public class Juego {
     public static void main(String[] args) {
         Buscaminas buscaminas = new Buscaminas();
-        buscaminas.jugar();
+        buscaminas.iniciar(); 
     }
 }
